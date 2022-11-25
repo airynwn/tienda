b8a664cc87d75335aa463862a5a4123b26aa7ac0
airynwn <laura.alcon@iesdonana.org>
2022-11-25 18:06:52 +0100

    pdf

--- public/index.php
+++ public/index.php
@@ -14,6 +14,10 @@
     <?php
     require_once '../src/auxiliar.php'; // carga el autoloader, que cargara  en todos
     // hay que serializar para no meter objeto en una sesion (carrito=var de sesion)
+
+    $mpdf = new \Mpdf\Mpdf();
+    
+    
     $carrito = unserialize(carrito());
 
     $pdo = conectar();
